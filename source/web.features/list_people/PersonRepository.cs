b8b795ec16a123fecea94c0e00b1e6b83eac190e
Tyler Rempel <tylerr@iqmetrix.com>
2017-03-22 17:58:31 -0500

    Pushing new changes

---
+++ source/web.features/list_people/PersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using code.prep.people;
+
+namespace code.web.features.list_people
+{
+    public class PersonRepository
+    {
+
+    }
+}
---
+++ source/web.features/list_people/PersonRepositorySpecs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using code.utility.matching;
+using Machine.Specifications;
+using spec = developwithpassion.specifications.use_engine<Machine.Fakes.Adapters.Rhinomocks.RhinoFakeEngine>;
+
+namespace code.web.features.list_people
+{
+    [Subject(typeof (PersonRepository))]
+    public class PersonRepositorySpecs
+    {
+        public abstract class concern : spec.observe<PersonRepository>
+        {
+
+        }
+
+
+        public class when_getting_all_people : concern
+        {
+            Establish c = () =>
+            {
+                request = fake.an<IProvideDetailsAboutAWebRequest>();
+                depends.on<IBuildAQueryFromARequest>();
+            };
+
+            Because b = () =>
+                sut.get_all_people(request);
+
+            private static IProvideDetailsAboutAWebRequest request;
+        }
+    }
+
+    internal interface IBuildAQueryFromARequest
+    {
+    }
+}
